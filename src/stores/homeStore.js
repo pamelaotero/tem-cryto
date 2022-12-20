a5c36b5f0999034704cbce19d39fc1c469d729f0
Pamela Otero <pamelasilvaoteroo@gmail.com>
2022-12-20 00:32:14 -0300

    Insert feature search by name and debounce

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -1,30 +1,56 @@
 import axios from 'axios'
 import create from 'zustand'
+import debounce from '../helpers/debounce'
 
 const homeStore = create((set) => ({
-
   coins: [],
+  query: '',
+
+  setQuery: (e) => {
+    set({ query: e.target.value })
+    homeStore.getState().searchCoins()
+  },
+
+  searchCoins: debounce( async () => {
+    const { query } = homeStore.getState()
+    
+    if (query.length > 2) {
+      const res = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`)
+
+      const coins = res.data.coins.map(coin => {
+        return {
+          name:coin.name,
+          image: coin.large,
+          id: coin.id
+        }
+      })
+
+      set({coins})
+    } else { 
+      
+    }
+
+  }, 500),
 
   fetchCoins: async() => {
-  
-  const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets/?vs_currency=usd')
-    const coins = res.data.map(coin => {
-      return {
-        marketCapRank: coin.market_cap_rank,
-        buy: coin?.roi?.currency || null,
-        id: coin.id,
-        name:  coin.name,
-        currency: coin.symbol,
-        current_price: coin.current_price,
-        priceChange24h: coin.price_change_24h,
-        marketCap: coin.market_cap,
-      }
-    }).slice(0,10)
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets/?vs_currency=usd')
+      const coins = res.data.map(coin => {
+        return {
+          marketCapRank: coin.market_cap_rank,
+          buy: coin?.roi?.currency || null,
+          id: coin.id,
+          name:  coin.name,
+          currency: coin.symbol,
+          current_price: coin.current_price,
+          priceChange24h: coin.price_change_24h,
+          marketCap: coin.market_cap,
+        }
+      }).slice(0,10)
 
-    set({coins})
-    // lista as top criptomoedas
-    console.log(coins)
-  }
+      set({coins})
+      // lista as top criptomoedas
+      console.log(coins)
+    }
 }))
 
 export default homeStore
